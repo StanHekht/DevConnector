fc619c48d596a4625191941024adcdabeeac8f94
Stanislau Hekht <stanhekht@Stanislaus-MBP.fios-router.home>
2020-10-25 02:00:14 -0400

    Implement user registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator');
+
+// Bring user model
+const User = require('../../models/User');
 
 // @route    POST api/users
 // @desc     Register user
@@ -15,13 +20,57 @@ router.post('/',
       'Please enter a password with 6 or more characters'
     ).isLength({ min: 6 })
 ], 
-(req, res) => {
+async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()){
         return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send('User route');
+    const { name, email, password } = req.body;
+
+    try {
+        let user = await User.findOne({ email });
+
+        // See if user exists
+        if (user) {
+        return res
+            .status(400)
+            .json({ errors: [{ msg: 'User already exists' }] });
+        }
+
+        // Get user's gravatar
+        const avatar = gravatar.url(email, {
+            s: '200',
+            r: 'pg',
+            d: 'mm'
+        })
+
+        // Create user instance
+        user = new User({
+            name,
+            email,
+            avatar,
+            password
+        });
+
+        // Encrypt password
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+
+        // Save user
+        await user.save();
+
+
+        // Return jsonwebtoken
+        // TODO
+
+        res.send('User registered');
+        
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+        
+    }
 })
 
 module.exports = router;

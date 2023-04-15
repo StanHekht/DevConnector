e28fd7838b13c19de55190d782ff0356a45c92d2
Stanislau Hekht <hekhtstanislau@gmail.com>
2023-04-15 16:33:18 -0400

    Normalize user avatar url and update express check syntax

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,6 +5,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
 const { check, validationResult } = require('express-validator');
+const normalize = require('normalize-url');
 
 // Bring in User model
 const User = require('../../models/User');
@@ -14,15 +15,12 @@ const User = require('../../models/User');
 // @access  Public
 router.post(
   '/',
-  [
-    check('name', 'Name is required').not().isEmpty(),
-    check('email', 'Please include a valid email').isEmail(),
-    check(
-      'password',
-      'Please enter a password with 6 or more characters'
-    ).isLength({ min: 6 }),
-  ],
-
+  check('name', 'Name is required').not().isEmpty(),
+  check('email', 'Please include a valid email').isEmail(),
+  check(
+    'password',
+    'Please enter a password with 6 or more characters'
+  ).isLength({ min: 6 }),
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -42,11 +40,14 @@ router.post(
       }
 
       // Get user's gravatar
-      const avatar = gravatar.url(email, {
-        s: '200', // default size
-        r: 'pg', // rating parental guidance
-        d: 'mm', // default image - user icon
-      });
+      const avatar = normalize(
+        gravatar.url(email, {
+          s: '200',
+          r: 'pg',
+          d: 'mm',
+        }),
+        { forceHttps: true }
+      );
 
       user = new User({
         name,

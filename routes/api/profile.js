98a93dd8432f3b82487a3d75e98bd418bf8ad0ed
Stanislau Hekht <hekhtstanislau@gmail.com>
2023-04-18 00:41:18 -0400

    Add route to delete user and profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -143,4 +143,24 @@ router.get('/user/:user_id', async (req, res) => {
   }
 });
 
+// @route    DELETE api/profile
+// @desc     Delete profile, user & posts
+// @access   Private
+router.delete('/', auth, async (req, res) => {
+  try {
+    // @todo - remove user's posts
+
+    // Remove profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+
+    // Remove user
+    await User.findOneAndRemove({ _id: req.user.id });
+
+    res.json({ msg: 'User deleted' });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;

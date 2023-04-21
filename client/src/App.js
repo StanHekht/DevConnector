b243e1868adb79e9075fa4787703c571dbef42b0
Stanislau Hekht <hekhtstanislau@gmail.com>
2023-04-21 03:17:52 -0400

    React router setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
 import { Fragment } from 'react';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import './App.css';
 
 const App = () => {
   return (
-    <Fragment>
-      <Navbar />
-      <Landing />
-    </Fragment>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Landing />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+      </Fragment>
+    </Router>
   );
 };
 

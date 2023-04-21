9356d821411df0f17daa477dacc9135a3ec5955f
Stanislau Hekht <hekhtstanislau@gmail.com>
2023-04-21 01:45:23 -0400

    Set up Navbar and Landing layout components with static markup

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import { Fragment } from 'react';
 import './App.css';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
 
 const App = () => {
   return (
     <Fragment>
-      <h1>App</h1>
+      <Navbar />
+      <Landing />
     </Fragment>
   );
 };

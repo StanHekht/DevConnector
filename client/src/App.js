00586f02143dc57d29050d808e12989f0dfe8202
Stanislau Hekht <hekhtstanislau@gmail.com>
2023-04-21 23:22:19 -0400

    Bring in redux, create store, redux middleware and bring in root reducer

--- client/src/App.js
+++ client/src/App.js
@@ -5,19 +5,24 @@ import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import './App.css';
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
 
 const App = () => {
   return (
-    <Router>
-      <Fragment>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<Landing />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/login' element={<Login />} />
-        </Routes>
-      </Fragment>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<Landing />} />
+            <Route path='/register' element={<Register />} />
+            <Route path='/login' element={<Login />} />
+          </Routes>
+        </Fragment>
+      </Router>
+    </Provider>
   );
 };
 

2521a3d11d6bfe307409ef23214aada437758672
Stanislau Hekht <hekhtstanislau@gmail.com>
2023-04-24 22:52:01 -0400

    Call alert action and begin work on Alert component

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import Alert from './components/layout/Alert';
 import './App.css';
 // Redux
 import { Provider } from 'react-redux';
@@ -15,6 +16,7 @@ const App = () => {
       <Router>
         <Fragment>
           <Navbar />
+          <Alert />
           <Routes>
             <Route path='/' element={<Landing />} />
             <Route path='/register' element={<Register />} />
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { setAlert } from '../../actions/alert';
+import PropTypes from 'prop-types';
 
-const Register = () => {
+const Register = ({ setAlert }) => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -18,7 +21,7 @@ const Register = () => {
   const onSubmit = async (e) => {
     e.preventDefault();
     if (password !== password2) {
-      console.log('Passwords do not match');
+      setAlert('Passwords do not match', 'danger');
     } else {
       console.log(formData);
     }
@@ -89,4 +92,8 @@ const Register = () => {
   );
 };
 
-export default Register;
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired,
+};
+
+export default connect(null, { setAlert })(Register);

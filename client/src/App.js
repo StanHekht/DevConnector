413707327620cb7090340e2cc06fa78cf82c9620
Stanislau Hekht <hekhtstanislau@gmail.com>
2023-05-06 00:26:11 -0400

    Create CreateProfile component

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/profile-forms/CreateProfile';
 import PrivateRoute from './components/routing/PrivateRoute';
 // Redux
 import { Provider } from 'react-redux';
@@ -37,6 +38,10 @@ const App = () => {
             <Route
               path='dashboard'
               element={<PrivateRoute component={Dashboard} />}
+            />
+            <Route
+              path='create-profile'
+              element={<PrivateRoute component={CreateProfile} />}
             />
           </Routes>
         </Fragment>

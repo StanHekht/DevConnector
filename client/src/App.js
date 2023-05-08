68cfbec1e1124ad80dbe1da1cd831f4a81801530
Stanislau Hekht <hekhtstanislau@gmail.com>
2023-05-08 00:31:21 -0400

    Experience/education: implement add experience and add education components

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,8 @@ import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/profile-forms/CreateProfile';
 import EditProfile from './components/profile-forms/EditProfile';
+import AddExperience from './components/profile-forms/AddExperience';
+import AddEducation from './components/profile-forms/AddEducation';
 import PrivateRoute from './components/routing/PrivateRoute';
 // Redux
 import { Provider } from 'react-redux';
@@ -47,6 +49,14 @@ const App = () => {
             <Route
               path='edit-profile'
               element={<PrivateRoute component={EditProfile} />}
+            />
+            <Route
+              path='add-experience'
+              element={<PrivateRoute component={AddExperience} />}
+            />
+            <Route
+              path='add-education'
+              element={<PrivateRoute component={AddEducation} />}
             />
           </Routes>
         </Fragment>
